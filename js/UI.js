7ac0639c915a4fd93a4af2509ffe89541f7936d9
wan-ching <80332166+wan-ching@users.noreply.github.com>
2021-03-10 00:23:15 +0800

    Add files via upload
    v1.0

---
+++ js/UI.js
@@ -0,0 +1,16 @@
+$('.mockup ul').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
+$('.mockup-one ul').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+});
